6943c8e40372e67dc2c6fdedc5b85c75b22efbd9
akhilesh0598 <akhilesh.chaurasiya0598@gmail.com>
2024-02-24 19:56:59 +0530

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -14,6 +14,14 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors(opt=>{
+    opt.AddPolicy("CorsPolicy",policy=>
+    {
+        policy.AllowAnyHeader().AllowAnyHeader().WithOrigins("http://localhost:3000");
+    }
+    );
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -23,6 +31,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 
 }
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
